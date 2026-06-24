791a825c3ea1d1a25769612edf88d195e3cd9dc1
Fidel Castro <fidelcastro4080@gmail.com>
2026-06-24 18:26:38 +0300

    function declaration and expression

--- src/11-function-declaration.ts
+++ src/11-function-declaration.ts
@@ -27,3 +27,29 @@ const sunglassesPrice: number = holidaySales("sunglasses", 10, 0.16, 0.12);
 const hatPrice: number = holidaySales("hats", 30, 0.16, 0.12);
 
 console.log(`The total price for sunglasses is $${hatPrice}`);
+
+// function expression
+let holidaySales1 = function (
+  product: string,
+  sales: number,
+  tax: number,
+  discount: number,
+): number {
+  let price: number = 0;
+
+  if (product === "sunglasses") {
+    price = 29;
+  } else if (product === "hats") {
+    price = 19;
+  } else if (product === "shoes") {
+    price = 59;
+  }
+
+  let totalSalesForIndividualProducts: number = price * sales;
+
+  return (
+    totalSalesForIndividualProducts -
+    tax * totalSalesForIndividualProducts -
+    discount * totalSalesForIndividualProducts
+  );
+};
