ffd586e105983ca56ef60f945dba2e0ffe587d68
Fidel Castro <fidelcastro4080@gmail.com>
2026-06-24 16:31:32 +0300

    if statements

---
+++ src/7-if-statement.ts
@@ -0,0 +1,15 @@
+let num1: number = 10;
+let num2: number = 20;
+let num3: number = 30;
+
+if (num1 > num2) {
+  console.log(`${num1} is greater than ${num2}`);
+} else {
+  console.log(`${num1} is less than ${num2}`);
+}
+
+// ternary operator
+
+num2 > num3
+  ? console.log(`${num2} is greater than ${num3}`)
+  : console.log(`${num2} is less than ${num3}`);
