34f83a428f0994d4735218d3313476a93cc60073
Fidel Castro <fidelcastro4080@gmail.com>
2026-06-24 11:47:46 +0300

    type: any

---
+++ src/3-any-type.ts
@@ -0,0 +1,8 @@
+//the type any is used when we dont know the data type of the variable decalred:
+
+let password;
+password = 23423;
+password = "23423";
+
+//it is not recommended since the core functionality or why ts was made is lost
+//AVOID USING IT AT ALL
