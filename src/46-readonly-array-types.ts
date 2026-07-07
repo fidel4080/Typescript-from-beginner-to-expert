ae4f77e18e4ad143f8b57d62457f2fc972427c2c
Fidel Castro <fidelcastro4080@gmail.com>
2026-07-07 13:20:57 +0300

    readonly arrays

---
+++ src/46-readonly-array-types.ts
@@ -0,0 +1,8 @@
+const numbers: ReadonlyArray<number> = [3, 4, 1, 2, 7];
+const strings: ReadonlyArray<string> = ["Hello", "TS"];
+
+// properties that change the array eg .push(), .sort() are unavailable in the readonlyArray type
+
+// properties available
+// .length
+// .indexOf()
